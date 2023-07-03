548f75d105b86d2407b7f3c1dd57d21ccf9b10c0
Mariana <marsmolina20@gmail.com>
2023-07-03 17:12:09 +0200

    add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-export default function HomePage() {
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function HomePage() {
+	const session = await getServerSession();
+	if (!session) {
+		return redirect('api/auth/signin');
+	}
 	return (
 		<div>
 			<h1>Home Page</h1>
