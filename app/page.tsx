0a6bc27f0de98bbfe87ec7283c35791735177cb1
Mariana <marsmolina20@gmail.com>
2023-07-04 14:45:51 +0200

    add prisma set up

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import { redirect } from 'next/navigation';
 
 export default async function HomePage() {
 	const session = await getServerSession();
+	console.log('HERE SESSION 😊😊😊', session);
 	if (!session) {
-		return redirect('api/auth/signin');
+		return <div>You need to signin</div>;
 	}
 	return (
 		<div>
--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -4,8 +4,6 @@ import { useSession } from 'next-auth/react';
 export default function AuthCheck({ children }: { children: React.ReactNode }) {
 	const { data: session, status } = useSession();
 
-	console.log('HERE SESION:', session, 'HERE STATUS:', status);
-
 	if (status === 'authenticated') {
 		return <>{children}</>;
 	} else {
